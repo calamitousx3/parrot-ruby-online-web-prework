3e13207ae5bf3e10748226686c1ff0324eb88c54
Carmen Renee <carmengenpt@gmail.com>
2019-01-06 21:00:25 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -2,9 +2,14 @@
 # returns the phrase
 
    
+<<<<<<< HEAD
   def parrot(name = "Squawk!")
     puts name
     name
+=======
+  def parrot(name = "Squawk")
+    puts name
+>>>>>>> f34e0b573d473179deb19debd2803c512ca64361
   
   end 
   parrot("Pretty bird!")
